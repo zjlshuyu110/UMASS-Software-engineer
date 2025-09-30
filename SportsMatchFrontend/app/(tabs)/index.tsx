fd7b7a41d6a270849a4e76e8e54c9a4720d554df
Kenneth <Kenneth012004@outlook.com>
2025-09-30 13:44:18 -0400

    Initial Nativewind setup

---
+++ SportsMatchFrontend/app/(tabs)/index.tsx
@@ -0,0 +1,12 @@
+// import "./global.css"
+import { Text, View } from "react-native";
+ 
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-white bg-blue-500 p-4 rounded">
+        Welcome to Nativewind!
+      </Text>
+    </View>
+  );
+}
