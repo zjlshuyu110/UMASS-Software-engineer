112437bbcca575a5b07d4f8c4c3ff3c36e6770fd
Kenneth <Kenneth012004@outlook.com>
2025-10-07 18:12:36 -0400

    Removed theme based UI

--- SportsMatchFrontend/app/(tabs)/_layout.tsx
+++ SportsMatchFrontend/app/(tabs)/_layout.tsx
@@ -12,7 +12,6 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
         tabBarButton: HapticTab,
       }}>
